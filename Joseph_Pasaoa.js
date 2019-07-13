cc0c86a5a9e1679d1b06552f39847e4fe11825dc
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-07-13 13:01:14 -0400

    adding Joseph_Pasaoa.js, v.1

---
+++ Joseph_Pasaoa.js
@@ -0,0 +1,6 @@
+let individual = {
+  firstName: "Joseph",
+  lastName: "Pasava",
+  DOB: "14-Feb-1978",
+  bio: "An early-career developer with the penchant for getting lost in loops and loops of loops that are lost in loops and loops of loops that are lost."
+}
