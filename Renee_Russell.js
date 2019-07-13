12fc6d199b0c995889c59d5487fc36084f915286
pursuitrenee <reneerussell@pursuit.org>
2019-07-13 13:01:30 -0400

    Renee profile

---
+++ Renee_Russell.js
@@ -0,0 +1,7 @@
+let person = {
+  firstName:'Renee',
+  lastName: 'Russell',
+  Dob:  07/22/1984,
+  Bio: 'I am an aspiring Full Stack Web Developer, mother, sister, daughter, wife, aunt, niece'
+
+}
