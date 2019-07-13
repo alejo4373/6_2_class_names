9b1f9d1bfc532a4cace87718995e5495e10988c0
DouglasMacKrell <douglasmackrell@pursuit.org>
2019-07-13 13:01:20 -0400

    please work

---
+++ Douglas_MacKrell.js
@@ -0,0 +1,10 @@
+let profile = {
+  firstName: "Douglas",
+  LastName: "MacKrell",
+  eyeColor: "Green",
+  dob: "5/28/1980",
+  bio: "Golden voiced video producer, maker, coder, and artesan. He/Him",
+  favoritePasta: "Wagon Wheel",
+  cats: 1,
+  married: "Very Yes",
+}
