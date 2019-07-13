cd8e779e464ecede652d48d0787e240723abf9e3
gsanchez13 <gisellesanchez@pursuit.org>
2019-07-13 12:57:52 -0400

    create person object

---
+++ giselle_sanchez.js
@@ -0,0 +1,7 @@
+let person = {
+  first : 'giselle',
+  last : 'sanchez',
+  dob : '11/13/1995',
+  bio :'Hi everyone, I\'m giselle. I like to bike, play soccer, go to the beach and be outdoors in general. '
+}
+console.log(person)
