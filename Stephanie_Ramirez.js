323873b6059ae5f348170a4dbfd657701eb084bf
serami12 <stephanieramirez1@pursuit.org>
2019-07-13 13:30:56 -0400

    test

--- Stephanie_Ramirez.js
+++ Stephanie_Ramirez.js
@@ -3,5 +3,5 @@ firstname:'Stephanie',
 lastname:'Ramirez',
 age:'25',
 dob:'09/14/1993'
-bio:'I am a very friendly anbd fun person who enjoys learning new things',
+bio:'I am a very friendly and fun person who enjoys learning new things',
 }
