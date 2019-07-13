cd50354593952107ac3fd43c925fb1ef44938f4a
Owen Jones <owenthefirst93@gmail.com>
2019-07-13 13:02:07 -0400

    added console.log

--- Owen_Jones.js
+++ Owen_Jones.js
@@ -4,3 +4,4 @@ let person = {
   dateOfBirth: 12071993
   bio:'I was born in Queens and raised in Pennsylvania. I moved back to NY when I graduated college and started working in tax accounting. Now I participate in a coding fellowship at Pursuit in LIC and Uber drive on the side. When I'm not working or coding, I enjoy playing soccer, hiking, surfing the web, and cooking.'
 }
+console.log(person);
