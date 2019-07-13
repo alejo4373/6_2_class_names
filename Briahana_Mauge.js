328da23711cb0561591f0fea987858fe6336fd66
Bpmauge <briahanamauge@pursuit.org>
2019-07-13 13:55:39 -0400

    Object Updated

--- Briahana_Mauge.js
+++ Briahana_Mauge.js
@@ -1,6 +1,7 @@
-let bri = {
+let briahana = {
 firstName:'Briahana',
 lastName: 'Maugé',
 dob: '03/01/1994',
-bio: 'Mathematics teacher, avid traveler, and current Pursuit 6.2 fellow.'
+bio: 'Mathematics teacher, world traveler, and current Pursuit 6.2 fellow.'
 }
+console.log(briahana);
