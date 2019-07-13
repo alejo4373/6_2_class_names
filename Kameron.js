a7639e8af595b9424aefab287b4006d86fb3b82a
KameronsCodeSpace <kameronmontague@pursuit.org>
2019-07-13 13:01:32 -0400

    Adding Kamerons\s object

---
+++ Kameron.js
@@ -0,0 +1,11 @@
+let me = {
+  firstname: 'Kameron',
+  lastname: 'Montague',
+  age: '32',
+  bio: 'I was born on a planet with no magic or fantastical creatures. Trapped within ' +
+  'the rules of this world which demand my ignorance of the universe around me, and ' +
+  'condemns me to a life of never knowing the answers to everything because of the scope ' +
+  'of infinity. Imagination is my only release from the mondane day to day.',
+}
+
+console.log(me);
