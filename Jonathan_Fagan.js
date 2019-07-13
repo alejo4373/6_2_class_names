5cf6eeaba6f8fc0fdc3d37f1b2699a9a437551fc
jonathan <jonathanfagan@pursuit.org>
2019-07-13 12:57:20 -0400

    Jonathan class exercise

---
+++ Jonathan_Fagan.js
@@ -0,0 +1,14 @@
+let jonathan = {
+  firstname: 'Jonathan',
+  lastname: 'Fagan',
+  nickname: 'Jaiden',
+  dob: '8/5/1986',
+  age: '32',
+  bio: "I was born in the bronx, lived most of my life with my grandmother until i was 8, " +
+  "i move in with my mother during that summer and continued life from there. I finished high " +
+  "school and went on to college to study for game design and programming. I still program alot " +
+  "I'm a father also and realized that part of being a good person is to always lead by inspiration "+
+  "when you inspire people to do better they become the best verions of themselves. And I live to see that."
+}
+
+console.log(jonathan);
